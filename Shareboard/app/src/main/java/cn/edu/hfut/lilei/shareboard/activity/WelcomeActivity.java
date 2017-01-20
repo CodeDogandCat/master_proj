1119ef1bef6b8a523409476319f83e68c5075033
李磊 <2662083658@qq.com>
2017-01-20 16:20:23 +0800

    第一次提交

---
+++ Shareboard/app/src/main/java/cn/edu/hfut/lilei/shareboard/activity/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.hfut.lilei.shareboard.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.edu.hfut.lilei.shareboard.R;
+
+
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
